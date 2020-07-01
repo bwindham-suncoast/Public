e271643d66352d01bca0a873e66eaef1a86aa819
bwindham-suncoast <67693897+bwindham-suncoast@users.noreply.github.com>
2020-07-01 09:39:46 -0400

    Update ReplaceeCWFile.ps1

--- ReplaceeCWFile.ps1
+++ ReplaceeCWFile.ps1
@@ -3,17 +3,17 @@
 #If it doesnt exist, it will write thst the path doesnt exist
 
 #variables
-$path = “C:\Program Files (x86)\eClinicalWorks”
+$path = "C:\Program Files (x86)\eClinicalWorks"
 $a = test-path -Path $path
 $networkShare = "\\nas\playground\IT\ERCActiveX.ocx"
-$destination = “C:\Program Files (x86)\eClinicalWorks\ERCActiveX.ocx“
+$destination = "C:\Program Files (x86)\eClinicalWorks\ERCActiveX.ocx"
 
 #test the path - if true
-if ($a -eq “True”) {
+if ($a -eq "True") {
 copy-item $networkShare -Destination $destination
 }
 
 #test the path - if false
 else {
-write-host ”$path does not exist on $env:computername”
+write-host "$path does not exist on $env:computername"
 }
